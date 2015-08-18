3d0f3de6fcb16020905da4b85c8ff6120a1f6a19
Alexey Tsymlov <alexey.tsymlov@gmail.com>
2015-08-18 16:26:18 +0300

    After 10th lecture.

--- Smashtag/TweetTableViewController.swift
+++ Smashtag/TweetTableViewController.swift
@@ -14,6 +14,7 @@ class TweetTableViewController: UITableViewController, UITextFieldDelegate {
     
     var searchText: String? = "#stanford" {
         didSet{
+            lastSuccessfulRequest = nil
             searchTextField?.text = searchText
             tweets.removeAll()
             tableView.reloadData()
@@ -36,23 +37,47 @@ class TweetTableViewController: UITableViewController, UITextFieldDelegate {
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
     
-    func refresh(){
-        if searchText != nil{
-            let request = TwitterRequest(search: searchText!, count: 100)
-            request.fetchTweets{ (newTweets)-> Void in
-                dispatch_async(dispatch_get_main_queue()){
-                    if newTweets.count > 0 {
-                        self.tweets.insert(newTweets, atIndex: 0)
-                        self.tableView.reloadData()
-                    }
-                }
+    var lastSuccessfulRequest: TwitterRequest?
+    
+    var nextRequestToAttempt: TwitterRequest? {
+        if lastSuccessfulRequest == nil {
+            if searchText != nil {
+                return TwitterRequest(search: searchText!, count: 100)
+            }else{
+                return nil
             }
+        } else{
+            return lastSuccessfulRequest?.requestForNewer
         }
+    }
+    
+    func refresh(){
+        if refreshControl != nil{
+            refreshControl?.beginRefreshing()
+        }
+        refresh(refreshControl)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    @IBAction func refresh(sender: UIRefreshControl?) {
+        if searchText != nil{
+            let request = nextRequestToAttempt
+            request!.fetchTweets{ (newTweets)-> Void in
+                dispatch_async(dispatch_get_main_queue()){
+                    if newTweets.count > 0 {
+                        self.lastSuccessfulRequest = request
+                        self.tweets.insert(newTweets, atIndex: 0)
+                        self.tableView.reloadData()
+                    }
+                    sender?.endRefreshing()
+                }
+            }
+        } else {
+            sender?.endRefreshing()
+        }
     }
     
     @IBOutlet weak var searchTextField: UITextField! {
